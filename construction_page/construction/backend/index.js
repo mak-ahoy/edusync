6ee580a429beb6d319647f463f8053ded44f88bd
unknown <muhammadalik1235@gmail.com>
2024-07-09 02:05:21 +0500

    deploy

--- construction_page/construction/backend/index.js
+++ construction_page/construction/backend/index.js
@@ -2,11 +2,10 @@
 
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const cors = require('cors');
 require('dotenv').config(); // For loading environment variables
 
 const app = express();
-const port = 8100;
+const port = 3000;
 
 const uri = process.env.MONGODB_URI; // MongoDB connection string from environment variables
 const client = new MongoClient(uri);
@@ -18,26 +17,29 @@ async function connectToMongoDB() {
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-}
-app.use(cors())
+} 
+
 app.use(express.json());
 
 // API endpoint to store email
-app.get('/hello', async (req, res)=>{
-  res.status(200).json({message : "Hello world"});
-})
 
+app.get('/hello', async (req, res) => {
+  try{
+    res.status(200).json({ message: 'Hello, World!' });
+
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+)
 app.post('/api/storeEmail', async (req, res) => {
   try {
     const { email } = req.body;
-
-    if (!email){
-      return res.status(400).json({ error: 'Email is required' });
-    }
     
     // Connect to MongoDB
     const db = client.db(process.env.DB_NAME);
-    const emailsCollection = db.collection('waitlist_emails');
+    const emailsCollection = db.collection('emails');
 
     // Store email in MongoDB
     const result = await emailsCollection.insertOne({ email });
