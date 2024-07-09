6a1e2ce477a870d040d37b7728cd6b71fec93531
Ali Haris <ali.haris@sixlogics.local>
2024-07-09 11:29:39 +0500

    to deploy

--- construction_page/construction/backend/index.js
+++ construction_page/construction/backend/index.js
@@ -1,14 +1,12 @@
-// Example backend using Node.js and Express
-
 const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config(); // For loading environment variables
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const uri = process.env.MONGODB_URI; // MongoDB connection string from environment variables
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToMongoDB() {
   try {
@@ -17,29 +15,37 @@ async function connectToMongoDB() {
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-} 
+}
 
+// Middleware
 app.use(express.json());
 
-// API endpoint to store email
-
-app.get('/hello', async (req, res) => {
-  try{
-    res.status(200).json({ message: 'Hello, World!' });
-
-  }
-  catch(error){
+// API endpoint to say hello
+app.get('/', async (req, res) => {
+  try {
+    res.status(200).json({ message: 'Welcome!' });
+  } catch (error) {
     console.log(error);
   }
-}
-)
+});
+
+// API endpoint to say hello
+app.get('/hello', async (req, res) => {
+  try {
+    res.status(200).json({ message: 'Hello, World!' });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// API endpoint to store email
 app.post('/api/storeEmail', async (req, res) => {
   try {
     const { email } = req.body;
-    
+
     // Connect to MongoDB
     const db = client.db(process.env.DB_NAME);
-    const emailsCollection = db.collection('emails');
+    const emailsCollection = db.collection('waitlist_emails');
 
     // Store email in MongoDB
     const result = await emailsCollection.insertOne({ email });
@@ -52,14 +58,16 @@ app.post('/api/storeEmail', async (req, res) => {
   }
 });
 
-// Start server and connect to MongoDB
-app.listen(port, async () => {
-  console.log(`Server running at http://localhost:${port}`);
-  await connectToMongoDB();
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
+// Connect to MongoDB when the server starts
+connectToMongoDB();
 
-// index.js
+
+// // index.js
 
 // const express = require('express');
 // const app = express();
